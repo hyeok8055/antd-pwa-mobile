c9334a54aa67e5fd4aa2fee14558fbc9fa1c75b1
hyeok8055 <juhyeok0123@gmail.com>
2024-12-20 08:58:51 +0000

    temp : test build

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -5,13 +5,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration using Vite's environment variables
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBqu5TDSwaY_qvunrS8pJrWdpIlwJeOMrU',
+  authDomain: 'calori-sync-f0431.firebaseapp.com',
+  projectId: 'calori-sync-f0431',
+  storageBucket: 'calori-sync-f0431.firebasestorage.app',
+  messagingSenderId: '830533101887',
+  appId: '  1:830533101887:web:5bd8aed35d49ea87758b8a',
+  measurementId: 'G-55K939QBD7',  
 };
 
 // Initialize Firebase
